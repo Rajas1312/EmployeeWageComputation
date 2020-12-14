c62022e631dadb521de875b2f377d588bff746a6
Rajas1312 <you@example.com>
2020-12-14 17:28:38 +0530

    Adde: part time employee wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,6 +3,7 @@ package employeeWage;
 public class EmployeeWage {
 
 	public static final int  IS_FULL_TIME=1;
+	public static final int  IS_PART_TIME=2;
 	public static final int EMP_RATE_PER_HOUR=20;
 	
 	public static void main(String[] args) {
@@ -10,10 +11,17 @@ public class EmployeeWage {
 		int empHrs=0;
 		int empWage=0;
 		
-		int empCheck=(int)(Math.floor(Math.random()*10)%2);
+		int empCheck=(int)(Math.floor(Math.random()*10)%3);
 		if(empCheck==IS_FULL_TIME) {
 			empHrs=8;
 			empWage=empHrs*EMP_RATE_PER_HOUR;
+		}
+		else if(empCheck==IS_PART_TIME) {
+			empHrs=4;
+			empWage=empHrs*EMP_RATE_PER_HOUR;
+		}
+		else {
+			empHrs=0;
 		}
 		System.out.println("Employee wage is "+empWage);
 
