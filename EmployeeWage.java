716f11905e2ef45a2415ef8af4612109c9ea55a2
Rajas1312 <you@example.com>
2020-12-14 16:23:42 +0530

    Added: code for attandance using random

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,9 +2,19 @@ package employeeWage;
 
 public class EmployeeWage {
 
+	public static final int  IS_FULL_TIME=1;
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to EmployeeWage Computation");
 		
+		int empCheck=(int)(Math.floor(Math.random()*10)%2);
+		if(empCheck==IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			
+			System.out.println("Employee is absent");
+		}
 
 	}
 
