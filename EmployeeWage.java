6d41ba070afa72c99b1fa91f50a62b998226d8a5
Rajas1312 <you@example.com>
2020-12-19 12:30:00 +0530

    Refactored:To save employee wage for multiple companies

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,16 +1,32 @@
 package employeeWage;
-
 public class EmployeeWage {
-
 	public static final int  IS_FULL_TIME=1;
 	public static final int  IS_PART_TIME=2;
 	
-	public static void main(String[] args) {
-		computeEmpWage("Dmart",20,10,10);
-		computeEmpWage("Relaince",20,20,10);
-		
+	public final String company;
+	public final int empRatePerHrs;
+	public final int noOfWorkingDays;
+	public final int maxHrsPerMonths;
+	public int totalEmpWage;
+	
+	public EmployeeWage(String company,int empRatePerHrs,int noOfWorkingDays,int maxHrsPerMonths) {
+		this.company=company;
+		this.empRatePerHrs=empRatePerHrs;
+		this.noOfWorkingDays=noOfWorkingDays;
+		this.maxHrsPerMonths=maxHrsPerMonths;
 	}
-	public static int computeEmpWage(String company,int empRatePerHrs,int noOfWorkingDays,int maxHrsPerMonths) {
+	
+	public static void main(String[] args) {
+	
+		EmployeeWage Dmart=new EmployeeWage("Dmart",20,2,10);
+		EmployeeWage Reliance=new EmployeeWage("Relaince",10,4,20);
+		Dmart.computeEmpWage();
+		Reliance.computeEmpWage();
+	
+	
+	}
+	
+	public int computeEmpWage() {
 		System.out.println("Welcome to EmployeeWage Computation");
 		int empHrs=0;
 		int empWage=0;
@@ -34,7 +50,6 @@ public class EmployeeWage {
 	 empWage=empHrs*empRatePerHrs;
 	 totalEmpWage+=empWage;
 	 System.out.println("Employe Wage is " + empWage);
-	 
 		}
 	System.out.println("Total wage for " +company + " is " +totalEmpWage);
 	return totalEmpWage;
